a0869cd736315df46e0f505c103fdcaa899ebc04
Nicolás <nicolascesarini94@hotmail.com>
2023-03-08 11:43:05 -0300

    Termino task2

--- assets/js/upcoming_events.js
+++ assets/js/upcoming_events.js
@@ -6,13 +6,17 @@ function mostrarCards(eventos) {
     for (card of eventos) {
         tarjetas += `
     <div class="card" style="width: 18rem;">
-      <img src=${card.image} class="card-img-top" alt="cinema">
-      <div class="card-body">
-        <p class="card-text">${card.date}</p>
-        <h5 class="card-title">${card.name}</h5>
-        <p class="card-text">${card.description}</p>
-        <p class="card-text">Price: ${card.price} U$D</p>
-        <a href="./details.html" class="btn btn-primary">Details</a>
+      <img src=${card.image} class="card-img-top img" alt="cinema">
+      <div class="card-body d-flex flex-column justify-content-between">
+            <div>
+                <p class="card-text">${card.date}</p>
+                <h5 class="card-title">${card.name}</h5>
+                <p class="card-text">${card.description}</p>
+            </div>
+            <div class="d-flex flex-column justify-content-between align-items-center">
+                <p class="card-text">Price: ${card.price} U$D</p>
+                <a href="./details.html" class="btn btn-primary">Details</a>
+            </div>
       </div>
     </div>`
     }
